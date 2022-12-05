c12ef1bcf41a43e8643da235a55316b2c7386067
Vlad P. Luchian <cstmlcodes@gmail.com>
2022-12-05 14:08:33 +0000

    update: assume the scripts are intended to be interpreted by bash

--- run-shell-check.sh
+++ run-shell-check.sh
@@ -2,6 +2,6 @@
 echo "> Running shell check on all the '.sh' scripts."
 find ./scripts \
   | grep -E ".*.sh" \
-  | xargs shellcheck \
+  | xargs shellcheck -s bash\
   > ./docs/audit/shell-check-report.txt
 echo "> Done."
