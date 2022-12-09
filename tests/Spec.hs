382dd8e37af13376206eb5ab1cc40832091f09d9
IAmPara0x <confusedstudent13@gmail.com>
2022-12-09 15:58:18 +0530

    remove redundant code

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -14,42 +14,6 @@ tests =
       [ simpleLockVesting
       , checkVestingUtxoExists
       , mkAndUnlockVesting
-         -- assertExecutionWith
-         --  [ShowTraceButOnlyContext ContractLog $ Error [AnyLog]]
-         --  "Get all vesting utxos"
-         --  (withCollateral $ initAda [1000])
-         --  (do
-         --    withContract $ \_ -> do
-         --        utxos <- Map.toList <$> getUtxos Vesting.vestingAddr
-         --        when (null utxos) $ error "Expected 1 or more utxos at vesting contract address"
-         --  )
-         --  [shouldSucceed]
-
-        -- let wallet0 = 200_000_000
-        --     wallet1 = 100_000_000
-        --     fundsToVest = Ada.lovelaceValueOf 100_000_000
-        -- in
-        --  assertExecutionWith
-        --   [ShowTraceButOnlyContext ContractLog $ Error [AnyLog]]
-        --   "Unlock vesting successfully"
-        --   (withCollateral $ initAda [wallet0] <> initAda [wallet1])
-        --   (do
-        --
-        --     execRes <- withContract $ \[w1pkh] -> do
-        --         (_, startTime) <- Contract.currentNodeClientTimeRange
-        --
-        --         let vestingDatum = Input [Ledger.unPaymentPubKeyHash w1pkh] [Vesting.Portion startTime fundsToVest]
-        --
-        --         void $ mkVesting vestingDatum
-        --         return vestingDatum
-        --
-        --     let (Right (vestingDatum, _)) = outcome execRes
-        --
-        --     withContractAs 1 $ \[_w0pkh] ->
-        --       makeAndUnlockVesting vestingDatum
-        --   )
-        --   [shouldSucceed]
-
       ]
 
 main :: IO()
